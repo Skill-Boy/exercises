8cf1f309739c319d00f7bd1a72f36b2fc8abe89a
Skill-Boy <erickakegil@outlook.es>
2019-12-12 12:56:50 -0600

    fixed

--- limits.rb
+++
@@ -1,6 +0,0 @@
-def limits(words, total_character)
-  word = words.join.gsub(/(.{#{total_character}})/, '\1 \2').gsub(" ", ",")
-  word.split(",")
-end  
-  
-limits(['hola', 'este', 'es', 'un', 'nuevo', 'mensaje'], 10)
